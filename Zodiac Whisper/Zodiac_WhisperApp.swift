51aad48e9e310eb455a7d2785876c69cc5974146
Ada <adka.nevatka>
2024-07-15 21:24:02 +0200

    Initial Commit

---
+++ Zodiac Whisper/Zodiac_WhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zodiac_WhisperApp.swift
+//  Zodiac Whisper
+//
+//  Created by Ada on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Zodiac_WhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Zodiac WhisperUITests/Zodiac_WhisperUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Zodiac_WhisperUITestsLaunchTests.swift
+//  Zodiac WhisperUITests
+//
+//  Created by Ada on 15/07/2024.
+//
+
+import XCTest
+
+final class Zodiac_WhisperUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
